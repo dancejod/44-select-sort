9da869df56a21a7c42aa017e834a231b035412eb
dancejod <dancejova.daniela@gmail.com>
2022-02-05 22:19:28 +0100

    Pridane komentare, kozmeticke upravy

--- select-sort.py
+++ select-sort.py
@@ -1,19 +1,23 @@
-array = [1,26,23,78,54,3,99]
+# Array, ktory chceme zoradit pomocou Selection Sort
+sample_array = [32, 1, 156, 4, 81, 52, 100]
 
-for item in range(len(array)-1):
-    selected_number = array[item]
-    lowest_number = array[item+1]
-    lowest_number_id = array.index(lowest_number)
+for index in range(len(sample_array)-1):
+    # Zvoli sa prvok v zozname a prvok po nom, ktory sa porovnava so zvyskom array v dalsom cykle
+    selected_number = sample_array[index]
+    lowest_number = sample_array[index+1]
+    lowest_number_id = index+1     
 
-    for number in range(item+1,len(array)):
-        compared_number = array[number]
+    for number in range(index+1,len(sample_array)):
+        # Postupne zmensovanie rozsahu zoznamu, hladanie najmensieho cisla v zvysku zoznamu
+        compared_number = sample_array[number]
         if lowest_number > compared_number:
             lowest_number = compared_number
-            lowest_number_id = array.index(lowest_number)
+            lowest_number_id = number
         
     if lowest_number < selected_number:
-        lowest_number, selected_number == selected_number, lowest_number
-        array[item] = lowest_number
-        array[lowest_number_id] = selected_number
+        # Prehodenie najmensieho cisla s prave zvolenym cislom v zozname podla indexov
+        sample_array[index] = lowest_number
+        sample_array[lowest_number_id] = selected_number
 
-print(array)
+# Zoradeny array
+print(sample_array)
