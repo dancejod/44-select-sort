19e71c85fa7f6c0e1da47bb56bd321557ee49e1b
dancejod <dancejova.daniela@gmail.com>
2022-02-05 21:26:48 +0100

    Opraveny selection sort, nefungoval pre vsetky arrays

--- select-sort.py
+++ select-sort.py
@@ -1,18 +1,20 @@
-array = [15, 6, 12, 56, 109, 81, 3]
+array = [36,51,11,2,72,16,100]
 
 for item in range(len(array)-1):
     selected_number = array[item]
     lowest_number = array[item+1]
+    lowest_number_id = array.index(lowest_number)
 
     for number in range(item+1,len(array)):
         compared_number = array[number]
         if lowest_number > compared_number:
             lowest_number = compared_number
+            lowest_number_id = array.index(lowest_number)
         
     if lowest_number < selected_number:
         lowest_number, selected_number == selected_number, lowest_number
         array[item] = lowest_number
-        array[number] = selected_number
+        array[lowest_number_id] = selected_number
 
 
     
